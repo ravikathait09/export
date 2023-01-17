c31c2a280f00be45ea2dc9fdef3f01a974852d4d
ravikathait09 <mac@Macs-MacBook-Air.local>
2023-01-18 03:02:24 +0530

    remove memory limit from index

--- public/index.php
+++ public/index.php
@@ -4,7 +4,6 @@ use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
-ini_set('memory_limit','1024M');
 /*
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
